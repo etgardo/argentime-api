fc8547a3057b59ab19797556eb6e432a2f1d324b
Edgardo <etgardo@gmail.com>
2022-10-17 09:34:18 -0400

    Rename folders name

--- src/suscribers/dto/update-suscriber.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSuscriberDto } from './create-suscriber.dto';
-
-export class UpdateSuscriberDto extends PartialType(CreateSuscriberDto) {}
--- src/suscribers/suscribers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SuscribersService } from './suscribers.service';
-import { SuscribersController } from './suscribers.controller';
-
-@Module({
-  controllers: [SuscribersController],
-  providers: [SuscribersService]
-})
-export class SuscribersModule {}
